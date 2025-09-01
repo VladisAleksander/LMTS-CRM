5b512e8dc69f46d80c8259d4635c682e90c9361a
VladisAleksander <vladis_alexander@hotmail.com>
2025-08-31 23:44:47 -0600

    Corrección menor de error en la línea 143 que impedía ver los detalles de los tickets en la implementación web

--- public/js/tickets/consult.js
+++ public/js/tickets/consult.js
@@ -140,8 +140,8 @@ $(document).ready(function(){
 
 function verTicket(tick_id) {
     // Redirigimos a la página de detalles del ticket con el ID del ticket
-    window.location.href = "../../soporte/DetalleTicket/?id=" + tick_id; // Redirigimos a la página de detalles del ticket
-    //window.open("http://localhost/LMTS-CRM/soporte/DetalleTicket/?id=" + tick_id, "_blank"); // Abrimos la página de detalles del ticket en una nueva pestaña
+    window.location.href = "../../Soporte/DetalleTicket/?id=" + tick_id; // Redirigimos a la página de detalles del ticket
+    //window.open("http://localhost/LMTS-CRM/Soporte/DetalleTicket/?id=" + tick_id, "_blank"); // Abrimos la página de detalles del ticket en una nueva pestaña
 }
 
 init();
