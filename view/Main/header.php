877f0dbac4f02303eac93a7359ccc2097116de73
VladisAleksander <vladis_alexander@hotmail.com>
2026-01-15 01:40:56 -0600

    Se añade el icono de notificaciones junto con su menu desplegable en el encabezado

--- view/Main/header.php
+++ view/Main/header.php
@@ -7,15 +7,68 @@
         </a>
 
         <button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
-            <span>toggle menu</span>
+            <span>Menu</span>
         </button>
 
         <button class="hamburger hamburger--htla">
-            <span>toggle menu</span>
+            <span>Menu</span>
         </button>
         <div class="site-header-content">
             <div class="site-header-content-in">
                 <div class="site-header-shown">
+
+                    <div class="dropdown dropdown-notification notif"> <!-- Notifications -->
+                        <a href="#"
+                            class="header-alarm dropdown-toggle active"
+                            id="dd-notification"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false">
+                            <i class="font-icon-alarm"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-notif" aria-labelledby="dd-notification">
+                            <div class="dropdown-menu-notif-header">
+                                Notificaciones
+                                <span class="label label-pill label-danger">4</span>
+                            </div>
+                            <div class="dropdown-menu-notif-list">
+                                <div class="dropdown-menu-notif-item">
+                                    <div class="photo">
+                                        <img src="/public/img/photo-64-1.jpg" alt="">
+                                    </div>
+                                    <div class="dot"></div>
+                                    <a href="#">Morgan</a> was bothering about something
+                                    <div class="color-blue-grey-lighter">7 hours ago</div>
+                                </div>
+                                <div class="dropdown-menu-notif-item">
+                                    <div class="photo">
+                                        <img src="/public/img/photo-64-2.jpg" alt="">
+                                    </div>
+                                    <div class="dot"></div>
+                                    <a href="#">Lioneli</a> had commented on this <a href="#">Super Important Thing</a>
+                                    <div class="color-blue-grey-lighter">7 hours ago</div>
+                                </div>
+                                <div class="dropdown-menu-notif-item">
+                                    <div class="photo">
+                                        <img src="/public/img/photo-64-3.jpg" alt="">
+                                    </div>
+                                    <div class="dot"></div>
+                                    <a href="#">Xavier</a> had commented on the <a href="#">Movie title</a>
+                                    <div class="color-blue-grey-lighter">7 hours ago</div>
+                                </div>
+                                <div class="dropdown-menu-notif-item">
+                                    <div class="photo">
+                                        <img src="/public/img/photo-64-4.jpg" alt="">
+                                    </div>
+                                    <a href="#">Lionely</a> wants to go to <a href="#">Cinema</a> with you to see <a href="#">This Movie</a>
+                                    <div class="color-blue-grey-lighter">7 hours ago</div>
+                                </div>
+                            </div>
+                            <div class="dropdown-menu-notif-more">
+                                <a href="#">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
 
                     <div class="dropdown user-menu"> <!-- User menu -->
                         <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
